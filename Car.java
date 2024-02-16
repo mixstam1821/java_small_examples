54ddbf208f7c127b7bc468d15f790f16ae48f122
Michael Stamatis <82336443+mixstam1821@users.noreply.github.com>
2024-02-16 23:44:00 +0200

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,22 @@
+public class Car {
+    String brand;
+    String model;
+    int year;
+
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void displayInfo() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+
+    public static void main(String[] args) {
+        Car myCar = new Car("Toyota", "Camry", 2022);
+        myCar.displayInfo();
+    }
+}
