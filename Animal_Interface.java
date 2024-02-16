792887056fc1e49dde7789ef9faae4770c1b8027
Michael Stamatis <82336443+mixstam1821@users.noreply.github.com>
2024-02-16 23:51:21 +0200

    Create Animal_Interface.java

---
+++ Animal_Interface.java
@@ -0,0 +1,27 @@
+interface Animal {
+    void makeSound();
+}
+
+class Dog implements Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Woof");
+    }
+}
+
+class Cat implements Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Meow");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        dog.makeSound(); // Output: Woof
+        cat.makeSound(); // Output: Meow
+    }
+}
